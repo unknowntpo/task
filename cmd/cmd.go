5c6713a6c35f16bc6694ebecc53655950bec43ef
Chang Chen Chien <e850506@gmail.com>
2021-03-06 16:59:05 +0800

    Rearrange cmd package
    This commit rearrange cmd package, users can use cmd.Init() to
    init all commands.
    It's easier for testing rather than using structure literal to init
    an cobra.Command.
    
    Reference:
        - [How to test CLI commands made with Go and Cobra](https://gianarb.it/blog/golang-mockmania-cli-command-with-cobra)

---
+++ cmd/cmd.go
@@ -0,0 +1,82 @@
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/unknowntpo/task/db"
+)
+
+func newRootCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "task",
+		Short: "Task is a cli task manager",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Do Stuff Here
+		},
+	}
+}
+
+func newAddCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "add",
+		Short: "Adds a task to your task list",
+		Run: func(cmd *cobra.Command, args []string) {
+			task := strings.Join(args, " ")
+			err := db.CreateTask(db.Db, task)
+			if err != nil {
+				fmt.Errorf("Fail to create task: %v", err)
+				return
+			}
+
+			fmt.Printf("Add \"%s\" to your task list.\n", task)
+		},
+	}
+}
+
+func newListCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "list",
+		Short: "List all tasks",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("list called")
+		},
+	}
+}
+func newDoCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "do",
+		Short: "Mark a task as complete",
+		Run: func(cmd *cobra.Command, args []string) {
+			var ids []int
+			for _, arg := range args {
+				id, err := strconv.Atoi(arg)
+				if err != nil {
+					fmt.Println("Failed to parse the argument: ", arg)
+				} else {
+					ids = append(ids, id)
+				}
+			}
+			fmt.Println("ids: ", ids)
+		},
+	}
+}
+
+// Init inits an root command and add all commands in cmd pkg.
+// return error if it exist
+func Init() error {
+	rootCmd := newRootCmd()
+	listCmd := newListCmd()
+	addCmd := newAddCmd()
+	doCmd := newDoCmd()
+
+	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(doCmd)
+
+	rootCmd.Execute()
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/unknowntpo/task/cmd"
+import (
+	"fmt"
+
+	"github.com/unknowntpo/task/cmd"
+	"github.com/unknowntpo/task/db"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	err := db.Init("./my.db")
+	if err != nil {
+		fmt.Errorf("Failed to init database: %v", err)
+	}
+
+	err = cmd.Init()
+	if err != nil {
+		fmt.Errorf("Failed to init cmd package: %v", err)
+	}
 }
